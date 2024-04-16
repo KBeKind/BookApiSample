38b5899f1a2b36f8f7cd910c0f98cdce41e96835
KBeKind <79383539+KBeKind@users.noreply.github.com>
2024-04-15 19:42:49 -0500

    Create GoogleTestBook.java

---
+++ GoogleTestBook.java
@@ -0,0 +1,232 @@
+package org.launchcode.library.models;
+
+import java.util.List;
+
+public class GoogleTestBook {
+
+    private List<GoogleBookItem> items;
+
+    public List<GoogleBookItem> getItems() {
+        return items;
+    }
+
+    public void setItems(List<GoogleBookItem> items) {
+        this.items = items;
+    }
+
+    public static class GoogleBookItem {
+        private String id;
+        private String etag;
+        private String selfLink;
+        private GoogleBookVolumeInfo volumeInfo;
+        private SaleInfo saleInfo;
+        private AccessInfo accessInfo;
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getEtag() {
+            return etag;
+        }
+
+        public void setEtag(String etag) {
+            this.etag = etag;
+        }
+
+        public String getSelfLink() {
+            return selfLink;
+        }
+
+        public void setSelfLink(String selfLink) {
+            this.selfLink = selfLink;
+        }
+
+        public GoogleBookVolumeInfo getVolumeInfo() {
+            return volumeInfo;
+        }
+
+        public void setVolumeInfo(GoogleBookVolumeInfo volumeInfo) {
+            this.volumeInfo = volumeInfo;
+        }
+
+        public SaleInfo getSaleInfo() {
+            return saleInfo;
+        }
+
+        public void setSaleInfo(SaleInfo saleInfo) {
+            this.saleInfo = saleInfo;
+        }
+
+        public AccessInfo getAccessInfo() {
+            return accessInfo;
+        }
+
+        public void setAccessInfo(AccessInfo accessInfo) {
+            this.accessInfo = accessInfo;
+        }
+    }
+
+    public static class GoogleBookVolumeInfo {
+        private String title;
+        private List<String> authors;
+        private String publisher;
+        private String publishedDate;
+        private String description;
+        private List<IndustryIdentifier> industryIdentifiers;
+        private List<String> categories;
+        private Double averageRating;
+        private Integer ratingsCount;
+        private String maturityRating;
+        private ImageLinks imageLinks;
+        private String language;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public List<String> getAuthors() {
+            return authors;
+        }
+
+        public void setAuthors(List<String> authors) {
+            this.authors = authors;
+        }
+
+        public String getPublisher() {
+            return publisher;
+        }
+
+        public void setPublisher(String publisher) {
+            this.publisher = publisher;
+        }
+
+        public String getPublishedDate() {
+            return publishedDate;
+        }
+
+        public void setPublishedDate(String publishedDate) {
+            this.publishedDate = publishedDate;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public List<IndustryIdentifier> getIndustryIdentifiers() {
+            return industryIdentifiers;
+        }
+
+        public void setIndustryIdentifiers(List<IndustryIdentifier> industryIdentifiers) {
+            this.industryIdentifiers = industryIdentifiers;
+        }
+
+        public List<String> getCategories() {
+            return categories;
+        }
+
+        public void setCategories(List<String> categories) {
+            this.categories = categories;
+        }
+
+        public Double getAverageRating() {
+            return averageRating;
+        }
+
+        public void setAverageRating(Double averageRating) {
+            this.averageRating = averageRating;
+        }
+
+        public Integer getRatingsCount() {
+            return ratingsCount;
+        }
+
+        public void setRatingsCount(Integer ratingsCount) {
+            this.ratingsCount = ratingsCount;
+        }
+
+        public String getMaturityRating() {
+            return maturityRating;
+        }
+
+        public void setMaturityRating(String maturityRating) {
+            this.maturityRating = maturityRating;
+        }
+
+        public ImageLinks getImageLinks() {
+            return imageLinks;
+        }
+
+        public void setImageLinks(ImageLinks imageLinks) {
+            this.imageLinks = imageLinks;
+        }
+
+        public String getLanguage() {
+            return language;
+        }
+
+        public void setLanguage(String language) {
+            this.language = language;
+        }
+    }
+
+    public static class IndustryIdentifier {
+        private String type;
+        private String identifier;
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public String getIdentifier() {
+            return identifier;
+        }
+
+        public void setIdentifier(String identifier) {
+            this.identifier = identifier;
+        }
+    }
+
+    public static class ImageLinks {
+        private String smallThumbnail;
+        private String thumbnail;
+
+        public String getSmallThumbnail() {
+            return smallThumbnail;
+        }
+
+        public void setSmallThumbnail(String smallThumbnail) {
+            this.smallThumbnail = smallThumbnail;
+        }
+
+        public String getThumbnail() {
+            return thumbnail;
+        }
+
+        public void setThumbnail(String thumbnail) {
+            this.thumbnail = thumbnail;
+        }
+    }
+
+    public static class SaleInfo {
+    }
+
+    public static class AccessInfo {
+    }
+}
